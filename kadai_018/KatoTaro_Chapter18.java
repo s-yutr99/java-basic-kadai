2da2b8513cd016bf0715b06115795fd593ec3bf2
s-yutr99 <yutr0709@icloud.com>
2025-09-19 23:11:17 +0900

    Add files via upload

---
+++ kadai_018/KatoTaro_Chapter18.java
@@ -0,0 +1,12 @@
+package kadai_018;
+
+public class KatoTaro_Chapter18 extends Kato_Chapter18 {
+
+		public void KatoTaro_Chapter18() {
+			givenName ="太郎";
+		}
+		 void eachIntroduce() {
+			System.out.println("私はjavaが得意です");
+	}
+
+}
---
+++ kadai_018/Kato_Chapter18.java
@@ -0,0 +1,19 @@
+package kadai_018;
+
+abstract public class Kato_Chapter18 {
+	public String familyname = "加藤";
+	public String givenName = "";
+	public String address = "東京都中野区⚪×";
+	
+	public void commonlntroduce() {
+		System.out.println("住所は" + address + "です");
+	}
+	
+	abstract  void eachlntroduce();
+	
+	public void execIntroduce() {
+		System.out.println("名前は" + familyname + givenName + "です");
+		commonlntroduce();
+		eachlntroduce();
+	}
+}
