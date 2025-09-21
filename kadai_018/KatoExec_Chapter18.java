03e8419ed52c4be57160fbb618d13c3b4340b4ee
s-yutr99 <yutr0709@icloud.com>
2025-09-21 16:37:16 +0900

    Add files via upload

--- kadai_018/KatoExec_Chapter18.java
+++ kadai_018/KatoExec_Chapter18.java
@@ -2,12 +2,20 @@ package kadai_018;
 
 public class KatoExec_Chapter18 {
     public static void main(String[] args) {
-        KatoTaro_Chapter18 katoTaro = new KatoTaro_Chapter18();
-        KatoIchiro_Chapter18 katoIchiro = new KatoIchiro_Chapter18();
-        KatoHanako_Chapter18 katoHanako = new KatoHanako_Chapter18();
+        KatoTaro_Chapter18 taro = new KatoTaro_Chapter18();
+        taro.setGivenName();
+        taro.execIntroduce();
 
-        katoTaro.execIntroduce();
-        katoIchiro.execIntroduce();
-        katoHanako.execIntroduce();
+      
+
+        KatoIchiro_Chapter18 ichiro = new KatoIchiro_Chapter18();
+        ichiro.setGivenName();
+        ichiro.execIntroduce();
+
+      
+
+        KatoHanako_Chapter18 hanako = new KatoHanako_Chapter18();
+        hanako.setGivenName();
+        hanako.execIntroduce();
     }
 }
--- kadai_018/KatoTaro_Chapter18.java
+++ kadai_018/KatoTaro_Chapter18.java
@@ -1,9 +1,8 @@
 package kadai_018;
 
 public class KatoTaro_Chapter18 extends Kato_Chapter18 {
-
-		public void KatoTaro_Chapter18() {
-			this.givenName ="太郎";
+		public void setGivenName() {
+			givenName ="太郎";
 		}
 		 void eachIntroduce() {
 			System.out.println("私はjavaが得意です");
